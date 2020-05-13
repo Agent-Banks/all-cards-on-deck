4001a525d768c891c5b4b745a84bbde018c3e5c6
CodyBanks <codydouglasbanks@gmail.com>
2020-05-13 16:56:30 -0400

    completed for statements for card value.

--- Program.cs
+++ Program.cs
@@ -8,8 +8,39 @@ namespace all_cards_on_deck
   {
     static void Main(string[] args)
     {
+      // list created for suits and ranks of cards
       var suits = new List<string>() { "Clubs", "Diamonds", "Hearts", "Spades" };
-      var ranks = new list<string>() { "2", "3", "4", "5", "6", "7", "8", "9", "10","Jack", "Queen", "King", "Ace",};
+      var ranks = new List<string>() { "2", "3", "4", "5", "6", "7", "8", "9", "10","Jack", "Queen", "King", "Ace",};
+      var deck = new List<string>();
+
+      //
+      for (var suitIndex = 0; suitIndex < suits.Count(); suitIndex++)
+      {
+       for (var rankIndex = 0; rankIndex < ranks.Count(); rankIndex++)
+       {
+          var currentRank = ranks[rankIndex];
+          var currentSuit = suits[suitIndex];
+          var currentCard = $"The {currentRank} of {currentSuit}";
+          deck.Add(currentCard);
+       }
+      }
+      
+      // make n = 52 since we are dealing with 52 elements
+      var n = 52;
+     //for firstIndex from n - 1 down to 1 do:
+     for (var firstIndex = n-1; firstIndex >= 1; firstIndex--)
+     {
+     //secondIndex = random integer that is greater than or equal to 0 and LESS than firstIndex
+       Random rnd = new Random();
+       var secondIndex = rnd.Next(firstIndex); 
+     //Now swap the values at firstIndex and secondIndex by doing this:
+
+     //firstValue = the value from items[firstIndex]
+     //secondValue = the value from items[secondIndex]
+     //items[firstIndex] = secondValue
+     //items[secondIndex] = firstValue
+     }
+     
 
 
     }
